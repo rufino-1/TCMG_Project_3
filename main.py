48f1536239444fd32aeae860253ffab64545b3b3
rufino-1 <60904017+rufino-1@users.noreply.github.com>
2020-02-17 13:37:24 -0600

    Checks if log is in directory

---
+++ main.py
@@ -0,0 +1,50 @@
+#### Header
+# Name:     Rufino A. Oregon
+# UIN:      826004309
+# CLASS:    TCMG 412
+# SECTION:  500
+# PROJECT:  3
+# DATE:     2/17/2020
+
+####Imports
+import os.path
+from os import path
+import requests
+####Functions
+
+####Main
+
+if(not(path.exists("log.txt"))):
+    print("Log file not found. Downloading log file.")
+    log_url = "https://s3.amazonaws.com/tcmg476/http_access_log"
+    r = requests.get(log_url)
+    with open("log.txt",'wb') as f: 
+        # Saving received content as a png file in 
+        # binary format 
+        # write the contents of the response (r.content) 
+        # to a new file in binary mode. 
+        f.write(r.content) 
+else:
+    print("Log file is in directory.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
